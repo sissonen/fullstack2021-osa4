5a8dcdb31b77a46d0ccecfacbfa014551d704180
Kalle Sissonen <kalle.sissonen@vincit.fi>
2021-12-14 14:37:11 +0200

    Controller and model added + required changes to app.js and index.js.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 console.log('Connecting to', config.MONGODB_URI)
@@ -16,5 +17,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
--- index.js
+++ index.js
@@ -3,34 +3,6 @@ const http = require('http')
 const config = require('./utils/config')
 const mongoose = require('mongoose')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log('Server running on port', config.PORT)
 })
