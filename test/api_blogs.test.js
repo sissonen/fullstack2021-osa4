d2b0127d6b6b10688b67cd80a9358ef298bce003
Kalle Sissonen <kalle.sissonen@vincit.fi>
2021-12-16 15:40:53 +0200

    Teht 4.10: blogilistan testit, step3

--- test/api_blogs.test.js
+++ test/api_blogs.test.js
@@ -12,17 +12,44 @@ beforeEach(async () => {
 })
 
 test('blog list', async () => {
+  
   const response = await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
   expect(response.body).toHaveLength(helper.testBlogs.length)
+
 })
 
 test('id is defined', async () => {
+  
   const response = await api
     .get('/api/blogs')
   expect(response.body[0].id).toBeDefined()
+
+})
+
+test('blog addition', async () => {
+  
+  const testEntry = {
+    author: 'Test',
+    title: 'Test',
+    url: 'test.com',
+    likes: 1
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(testEntry)
+    .expect(201)
+
+  const response = await api.get('/api/blogs')
+  
+  expect(response.body).toHaveLength(helper.testBlogs.length + 1)
+
+  const authors = response.body.map(blog => blog.author)
+  expect(authors).toContain(testEntry.author)
+
 })
 
 afterAll(() => {
