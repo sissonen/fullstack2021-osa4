4e112d63f15ea33d40646b71bc2ed0b889e1b040
Kalle Sissonen <kalle.sissonen@vincit.fi>
2021-12-16 13:58:23 +0200

    Teht 4.8: blogilistan testit, step 1

---
+++ test/api_blogs.test.js
@@ -0,0 +1,24 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.testBlogs)
+})
+
+test('blog list', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  expect(response.body).toHaveLength(helper.testBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
