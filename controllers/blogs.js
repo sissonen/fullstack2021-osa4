03ffa0316dfc827affba8677a587d20581633e70
Kalle Sissonen <kalle.sissonen@vincit.fi>
2021-12-17 17:47:15 +0200

    Teht 4.17: blogilistan laajennus, step5

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,19 +1,41 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
+  
+  const randUser = await User.find({})
+  if (randUser[0] === 'undefined') {
+    return response.status(400).json({ error: 'No user defined' })
+  }
+  const user = randUser[0]
+  const userId = randUser[0].id
+  
   if (typeof request.body.title === 'undefined' && typeof request.body.url === 'undefined') {
-    response.status(400).end()
-  } else {
-    const blog = new Blog(request.body)
 
-    const result = await blog.save()
-    response.status(201).json(result)
+    return response.status(400).end()
+
+  } else {
+
+    const blog = new Blog({
+      author: request.body.author,
+      title: request.body.title,
+      url: request.body.url,
+      user: userId
+    })
+
+    const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
+
   }
 })
 
