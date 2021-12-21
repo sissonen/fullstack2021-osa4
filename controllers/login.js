788f08495c9e72b26ac2bd5ff111604753ff96b9
Kalle Sissonen <kalle.sissonen@vincit.fi>
2021-12-21 15:00:31 +0200

    Teht 4.18: blogilistan laajennus, step6

---
+++ controllers/login.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  const user = await User.findOne({ username: request.body.username })
+  
+  let passCorrect = false
+  if (user !== null) {
+    passCorrect = await bcrypt.compare(request.body.password, user.passwordHash)
+  } else {
+    passCorrect = false
+    console.log('No user found')
+  }
+
+  if (!passCorrect) {
+    return response.status(401).json({ error: 'Invalid username or password' })
+  }
+
+  const userInfo = {
+    usernam: user.username,
+    id: user._id
+  }
+
+  const token = jwt.sign(userInfo, process.env.SECRET)
+
+  response.status(200).send({ token, username: user.username, name: user.name })
+
+})
+
+module.exports = loginRouter
